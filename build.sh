edc9d58d9aa56ac91dbc2f378a1018e4c82f5951
Gero Kindling <gero.kindling@web.de>
2025-01-26 19:16:39 +0100

    Fix build script

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@
 # Build .exe for windows
 GOOS=windows GOARCH=amd64 go build -o bin/xplane-gateway-downloader.exe .
 
-# go into bin folder to avoid bin folder appearing in .zip file
+# Go into bin folder to avoid bin folder appearing in .zip file
 cd bin/
 
-# zip windows application
-tar -cvf xplane-gateway-downloader-windows.zip xplane-gateway-downloader.exe
+# Zip windows application
+zip xplane-gateway-downloader-windows.zip xplane-gateway-downloader.exe
 
-# get back out for next go build cmd
+# Get back out for next go build cmd
 cd ../
 
 
@@ -20,16 +20,16 @@ cd ../
 # Build for mac 64-bit
 GOOS=darwin GOARCH=amd64 go build -o bin/xplane-gateway-downloader .
 
-# go into bin folder to avoid bin folder appearing in .zip file
+# Go into bin folder to avoid bin folder appearing in .zip file
 cd bin/
 
-# zip mac 64-bit application
-tar -cvf xplane-gateway-downloader-mac_os.zip xplane-gateway-downloader
+# Zip mac 64-bit application
+zip xplane-gateway-downloader-mac_os.zip xplane-gateway-downloader
 
-# rename file
+# Rename file
 mv xplane-gateway-downloader xplane-gateway-downloader-amd64-darwin
 
-# get back out for next go build cmd
+# Get back out for next go build cmd
 cd ../
 
 
@@ -38,16 +38,16 @@ cd ../
 # Build for linux 32-bit
 GOOS=linux GOARCH=386 go build -o bin/xplane-gateway-downloader .
 
-# go into bin folder to avoid bin folder appearing in .zip file
+# Go into bin folder to avoid bin folder appearing in .zip file
 cd bin/
 
-# zip mac 64-bit application
-tar -cvf xplane-gateway-downloader-linux-32bit.zip xplane-gateway-downloader
+# Zip linux 32-bit application
+zip xplane-gateway-downloader-linux-32bit.zip xplane-gateway-downloader
 
-# rename file
+# Rename file
 mv xplane-gateway-downloader xplane-gateway-downloader-386-linux
 
-# get back out for next go build cmd
+# Get back out for next go build cmd
 cd ../
 
 
@@ -56,14 +56,14 @@ cd ../
 # Build for linux 64-bit
 GOOS=linux GOARCH=amd64 go build -o bin/xplane-gateway-downloader .
 
-# go into bin folder to avoid bin folder appearing in .zip file
+# Go into bin folder to avoid bin folder appearing in .zip file
 cd bin/
 
-# zip mac 64-bit application
-tar -cvf xplane-gateway-downloader-linux-64bit.zip xplane-gateway-downloader
+# Zip linux 64-bit application
+zip xplane-gateway-downloader-linux-64bit.zip xplane-gateway-downloader
 
-# rename file
+# Rename file
 mv xplane-gateway-downloader xplane-gateway-downloader-amd64-linux
 
-# get back out for next go build cmd
+# Get back out for next go build cmd
 cd ../
